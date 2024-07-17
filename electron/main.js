dbe351060dcb108643bea6dceec51b69652d02d4
C4skg <60612174+C4skg@users.noreply.github.com>
2024-07-17 12:27:36 +0800

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -7,82 +7,86 @@ const url = require('url');
 const iconPath = path.join(__dirname,'../src/assets/logo.png');
 
 let mainWindow,
-    tray;
+tray;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1250,
-    height: 800,
-    icon: iconPath,
-    webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-    },
-    titleBarStyle: 'hidden',
-    titleBarOverlay: {
-      color: 'rgba(0,0,0,0)',
-      height: 35,
-      symbolColor: 'white'
-    }
-  });
-  mainWindow.loadFile(
-    path.join(__dirname,"../dist/index.html")
-  );
+    mainWindow = new BrowserWindow({
+        width: 1250,
+        height: 800,
+        icon: iconPath,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        },
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: 'rgba(0,0,0,0)',
+            height: 35,
+            symbolColor: 'white'
+        }
+    });
+    mainWindow.loadFile(
+        path.join(__dirname,"../dist/index.html")
+    );
 
-  mainWindow.on('will-resize',function(event,newBounds,details){
-    if(
-      newBounds.width <= 1000
-      ||
-      newBounds.height <= 600
-    ){
-      event.preventDefault();
-    }
-  })
+    mainWindow.on('will-resize',function(event,newBounds,details){
+        if(
+            newBounds.width <= 1000
+            ||
+            newBounds.height <= 600
+        ){
+            event.preventDefault();
+        }
+    })
 
-  mainWindow.on('close',function(event){
-    new Notification({
-      title: "QickStudy已隐藏到托盘",
-    }).show();
-    event.preventDefault();
-    mainWindow.hide();
-  })
+    mainWindow.on('close',function(event){
+        if(!mainWindow.isFocused()){
+            'quit'
+        }else{
+            new Notification({
+                title: "QickStudy已隐藏到托盘",
+            }).show();
+            event.preventDefault();
+            mainWindow.hide();
+        }
+    })
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
 
-  // mainWindow.webContents.openDevTools()
-  
+    // mainWindow.webContents.openDevTools()
+
 }
 
 app.on('ready', createWindow);
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 app.whenReady().then(()=>{
-  tray = new Tray(iconPath);
-  const contextMenu = Menu.buildFromTemplate([
+    tray = new Tray(iconPath);
+    const contextMenu = Menu.buildFromTemplate([
     {
-      label: '设置',
-      click: function () {
-          console.log('setting')
-      }
+        label: '设置',
+        click: function () {
+            console.log('setting')
+        }
     },
     {
-      label: '退出',
-      click: function () {
-          if (process.platform !== 'darwin') {
-              app.quit();
-          }
-      }
+        label: '退出',
+        click: function () {
+            if (process.platform !== 'darwin') {
+                app.quit();
+            }
+        }
     }
-  ]);
-  tray.setToolTip('QickStudy');
-  tray.setContextMenu(contextMenu);
-  tray.on('double-click',function(){
+    ]);
+    tray.setToolTip('QickStudy');
+    tray.setContextMenu(contextMenu);
+    tray.on('double-click',function(){
     mainWindow.show();
-  })
+    })
 })
