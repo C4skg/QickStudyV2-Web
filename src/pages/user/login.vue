457d4d3e3da8ef73edd534c5096299c6b468e25a
C4skg <60612174+C4skg@users.noreply.github.com>
2024-08-04 12:16:36 +0800

    update
    1. 更新设置页面
    2. 更新登录、注册页面逻辑并完善
    3. 更新全局 css 样式

--- src/pages/user/login.vue
+++ src/pages/user/login.vue
@@ -1,40 +1,29 @@
 <template>
     <div class="login">
-        <v-img 
-            class="mx-auto my-6"
-            max-width="280"
-            :src="logo"
-        ></v-img>
+        <v-img class="mx-auto my-6" max-width="280" :src="logo"></v-img>
 
-        <v-card class="mx-auto pa-10 pb-8" elevation="8" max-width="448" rounded="lg">
-            <div class="text-subtitle-1 text-medium-emphasis">邮箱</div>
-            <v-text-field density="compact" placeholder="xxxx@xx.xx" prepend-inner-icon="mdi-email-outline"
-                variant="outlined"
-                type="email"
-                :rules="[rules.required, rules.email]"
-            ></v-text-field>
+        <v-card class="mx-auto pa-10 pb-6" elevation="8" max-width="450" min-width="400" width="450" rounded="lg">
+            <v-form
+                v-model="forms"
+                @submit.prevent="doLogin"
+            >
+                <v-text-field class="mb-3" density="compact" placeholder="请输入邮箱号" prepend-inner-icon="mdi-email-outline"
+                    variant="outlined" type="email" :rules="[rules.requiredEmial, rules.email]" v-model="userEmail" ></v-text-field>
 
-            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                密码
+                <v-text-field class="mb-3" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="visible ? 'text' : 'password'" density="compact" placeholder="登录密码"
+                    prepend-inner-icon="mdi-lock-outline" variant="outlined" @click:append-inner="visible = !visible"
+                    :rules="[rules.requiredPassword]" v-model="userPassword" ></v-text-field>
 
-                <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer"
-                    target="_blank">
-                    忘记密码?</a>
-            </div>
+                <div class="forget d-flex justify-end mb-3">
+                    <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer"
+                        target="_blank">忘记密码?</a>
+                </div>
 
-            <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
-                density="compact" placeholder="登录密码" prepend-inner-icon="mdi-lock-outline"
-                variant="outlined" @click:append-inner="visible = !visible"></v-text-field>
-
-            <v-card class="mb-12" color="surface-variant" variant="tonal">
-                <v-card-text class="text-medium-emphasis text-caption">
-                    登录后，默认代表您同意了 "快学(QickStudy)平台用户协议" 的相关协议规定，如果存在问题或需要帮助，请点击此处访问帮助中心
-                </v-card-text>
-            </v-card>
-
-            <v-btn class="mb-8" color="blue" size="large" variant="tonal" block>
-                登 录
-            </v-btn>
+                <v-btn class="mb-8 m" color="blue" size="large" variant="tonal" block :loading="loading" type="submit">
+                    登 录
+                </v-btn>
+            </v-form>
 
             <v-card-text class="text-center">
                 <RouterLink to="/register" class="text-blue text-decoration-none">
@@ -43,17 +32,10 @@
             </v-card-text>
 
             <div class="toolbar">
-                <v-btn
-                    class="overlayActivator"
-                    icon="mdi-cog"
-                >
-                    
+                <v-btn class="overlayActivator" icon="mdi-cog">
+
                 </v-btn>
-                <v-overlay
-                    activator=".overlayActivator"
-                    scroll-strategy="none"
-                    class="align-center justify-center"
-                >
+                <v-overlay activator=".overlayActivator" scroll-strategy="none" class="align-center justify-center">
                     <settings />
                 </v-overlay>
             </div>
@@ -67,24 +49,33 @@
     import settings from '@/layouts/settings.vue'
     import { ref } from 'vue'
 
-    const visible = ref(false);
+    const visible = ref(false),
+          userEmail = ref(null),
+          userPassword = ref(null),
+          forms = ref(false),
+          loading = ref(false);
 
     const rules = {
-        required: value => !!value || '请输入邮箱号',
+        requiredEmial: value => !!value || '请输入邮箱号',
+        requiredPassword: value => !!value || '请输入密码',
         email: value => {
             const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             return pattern.test(value) || '无效邮箱号'
         }
     };
 
-    
+    const doLogin = function(e){
+        if( !forms.value ) return false;
+        console.log(userEmail.value,userPassword.value)
+        loading.value = true;
+    }
 </script>
 
 <style scoped>
-.login{
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%,-50%);
-}
+    .login {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
 </style>
--- src/pages/user/register.vue
+++ src/pages/user/register.vue
@@ -1,92 +1,95 @@
 <template>
-    <div class="register">
-        <v-img
-            class="mx-auto my-6"
-            max-width="228"
-            :src="logo"
-        ></v-img>
+    <div class="login">
+        <v-img class="mx-auto my-6" max-width="280" :src="logo"></v-img>
 
-        <v-card
-            class="mx-auto pa-12 pb-8"
-            elevation="8"
-            max-width="448"
-            rounded="lg"
-        >
-            <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-
-            <v-text-field
-            density="compact"
-            placeholder="Email address"
-            prepend-inner-icon="mdi-email-outline"
-            variant="outlined"
-            ></v-text-field>
-
-            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-            Password
-
-            <a
-                class="text-caption text-decoration-none text-blue"
-                href="#"
-                rel="noopener noreferrer"
-                target="_blank"
+        <v-card class="mx-auto pa-10 pb-6" elevation="8" max-width="450" min-width="400" width="450" rounded="lg">
+            <v-form
+            v-model="forms"
+            @submit.prevent="doRegister"
             >
-                Forgot login password?</a>
-            </div>
+                <v-text-field class="mb-3" density="compact" placeholder="请输入邮箱号" prepend-inner-icon="mdi-email-outline"
+                    variant="outlined" type="email" :rules="[rules.requiredEmial, rules.email]"
+                    v-model="userEmail" :readonly="loading"></v-text-field>
 
-            <v-text-field
-            :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-            :type="visible ? 'text' : 'password'"
-            density="compact"
-            placeholder="Enter your password"
-            prepend-inner-icon="mdi-lock-outline"
-            variant="outlined"
-            @click:append-inner="visible = !visible"
-            ></v-text-field>
+                <v-text-field class="mb-3" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="visible ? 'text' : 'password'" density="compact" placeholder="登录密码"
+                    prepend-inner-icon="mdi-lock-outline" variant="outlined" @click:append-inner="visible = !visible"
+                    :rules="[rules.requiredPassword,rules.password]" v-model="userPassword" :readonly="loading"></v-text-field>
 
-            <v-card
-            class="mb-12"
-            color="surface-variant"
-            variant="tonal"
-            >
-            <v-card-text class="text-medium-emphasis text-caption">
-                Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
-            </v-card-text>
-            </v-card>
+                <v-text-field class="mb-3" :append-inner-icon="visible2 ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="visible2 ? 'text' : 'password'" density="compact" placeholder="确认密码"
+                    prepend-inner-icon="mdi-lock-outline" variant="outlined" @click:append-inner="visible2 = !visible2"
+                    :rules="[rules.requiredPassword,rules.equalPassword]" v-model="userConfirmPassword" :readonly="loading"></v-text-field>
 
-            <v-btn
-            class="mb-8"
-            color="blue"
-            size="large"
-            variant="tonal"
-            block
-            >
-            Log In
-            </v-btn>
+                <div class="forget d-flex justify-end mb-3">
+                    <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer"
+                        target="_blank">忘记密码?</a>
+                </div>
+
+                <v-btn class="mb-8 m" color="blue" size="large" variant="elevated" block
+                    :loading="loading"
+                    type="submit"
+                >
+                    注 册
+                </v-btn>
+            </v-form>
 
             <v-card-text class="text-center">
-            <a
-                class="text-blue text-decoration-none"
-                href="#"
-                rel="noopener noreferrer"
-                target="_blank"
-            >
-                Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-            </a>
+                <RouterLink :to="{name: 'login'}" class="text-blue text-decoration-none">
+                    前往登录 <v-icon icon="mdi-chevron-right"></v-icon>
+                </RouterLink>
             </v-card-text>
+
+            <div class="toolbar">
+                <v-btn class="overlayActivator" icon="mdi-cog"></v-btn>
+                <v-overlay activator=".overlayActivator" scroll-strategy="none" class="align-center justify-center">
+                    <settings />
+                </v-overlay>
+            </div>
         </v-card>
     </div>
 </template>
 
+
 <script setup>
     import logo from '@/assets/textlogo2x.png'
+    import settings from '@/layouts/settings.vue'
+    import { ref } from 'vue'
+
+    const visible = ref(false),
+          visible2 = ref(false),
+          forms = ref(false),
+          userEmail = ref(null),
+          userPassword = ref(null),
+          userConfirmPassword = ref(null),
+          loading = ref(false);
+
+    const rules = {
+        password: value => {
+            const pattern = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,}$/;
+            return pattern.test(value) || '密码长度至少6位且由字母数字组成'
+        },
+        requiredEmial: value => !!value || '请输入邮箱号',
+        requiredPassword: value => !!value || '请输入密码',
+        equalPassword: value => value == userPassword.value || '两次密码不一致',
+        email: value => {
+            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            return pattern.test(value) || '无效邮箱号'
+        }
+    };
+
+    const doRegister = function () {
+        if (!forms.value) return false;
+        loading.value = true;
+        // do register
+    }
 </script>
 
-
 <style scoped>
-.register{
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%,-50%);
-}
+    .login {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
 </style>
