1054cafcf516d3912aeff2e3605a434126f83823
C4skg <60612174+C4skg@users.noreply.github.com>
2024-12-30 21:51:16 +0800

    重大更新
    再阅读了一些优秀项目后，我认为该项目写的框架不够符合现代化的特征，文件设计等不合理，代码过于冗余，耦合度高，因此决定重新设计，思想延续，老的版本已经归档在 archives 分支中

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,5 @@
 </template>
 
 <script setup>
-    import { useRouter } from 'vue-router'
-    import { useSettingStore } from '@/stores/setting.js'
-    import { useTheme } from 'vuetify'
 
-    const router = useRouter(),
-          settingStore = useSettingStore(),
-          theme = useTheme();
-    // set theme
-    console.log(settingStore.theme);
-    theme.global.name.value = settingStore.theme;
-    localStorage.setItem('isLogined', true);
 </script>
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,11 +8,9 @@
 import vuetify from './vuetify'
 import pinia from '@/stores'
 import router from '@/router'
-import overrides from '@/overrides'
 
 export function registerPlugins(app) {
     app
-        .use(overrides)
         .use(pinia)
         .use(vuetify)
         .use(router)
